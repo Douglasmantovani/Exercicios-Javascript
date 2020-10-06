9c5763a46d505b306d1230fc21a16fc2efa0a78c
Douglasmantovani <almagedomchaotic19@gmail.com>
2020-10-06 13:21:57 -0300

    JS

---
+++ Timer.js
@@ -0,0 +1,10 @@
+function ExibirAlgo(){
+    console.log("Olá mundo");
+}
+
+//Exibira a mensagem a cada 1seg infinitamente sem uma consdicao de pausar---A function podera ter parametro
+setInterval(ExibirAlgo,1000)
+
+
+//Exibira a mensagem depois de um tempo determinado uma vez
+setTimeout(ExibirAlgo,5000)
